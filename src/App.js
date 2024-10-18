2cf36a71c3947f7f6602c5ed83467cf0dc6d8d78
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2024-10-19 02:08:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
+import EmailList from './components/EmailList';
+import EmailBody from './components/EmailBody';
+import Filter from './components/Filter';
 import './App.css';
 
 function App() {
+  const [selectedEmailId, setSelectedEmailId] = useState(null);
+  const [filter, setFilter] = useState('all');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Filter setFilter={setFilter} />
+      <div className="app-content">
+        <EmailList onEmailSelect={setSelectedEmailId} filter={filter} />
+        {selectedEmailId && <EmailBody selectedEmailId={selectedEmailId} />}
+      </div>
     </div>
   );
 }
